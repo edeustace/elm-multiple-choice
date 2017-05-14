fd9c9a83d80cf9530c8337ba5a7c6544f8f174ec
edeustace <ed.eustace@gmail.com>
2017-05-14 17:28:28 +0100

    tweaks

--- src/MultipleChoice.elm
+++ src/MultipleChoice.elm
@@ -2,7 +2,7 @@ module MultipleChoice exposing (..)
 import ListUtils exposing (mapFirst, mapExceptFirst)
 
 import Html exposing (Html, button, div, hr, label, text, input)
-import List exposing (map)
+import List exposing (map, member)
 import Html.Attributes exposing (name, style, type_, value, checked, disabled)
 import Html.Events exposing (onClick)
 
@@ -11,85 +11,80 @@ type Correctness = Correct | Incorrect | Unknown
 type Mode = Radio | Checkbox 
 
 type alias Choice = {
-  label : String
-  , value : String
-  , selected : Bool
-  , correctness : Correctness
-  , feedback : Maybe String
+  label : String,
+  value : String
 }
 
-type alias Data = {
+type alias Model = {
   prompt: String,
   mode : Mode,
-  choices: List Choice
+  choices: List Choice,
+  session : Session
 }
 
-type alias RawChoice = {
-  label : String
-  , value: String 
-  , selected: Bool 
-  , correctness: String
-  , feedback: Maybe String 
+type alias Session = {
+  value : List String 
 }
 
-type alias RawData = {
-  prompt: String, 
-  mode: String,
-  choices: List RawChoice 
-}
-      
   
-deselectAllButFirst : Mode -> Data -> Data
-deselectAllButFirst mode d =
-    let
-      isSelected = (\c -> c.selected == True) 
-      deselect = (\c -> {c | selected = False}) 
-      newChoices = mapExceptFirst isSelected deselect d.choices
-    in
-      { d | choices = newChoices }
+-- deselectAllButFirst : Mode -> Model -> Model  
+-- deselectAllButFirst mode d =
+--     let
+--       isSelected = (\c -> c.selected == True) 
+--       deselect = (\c -> {c | selected = False}) 
+--       newChoices = mapExceptFirst isSelected deselect d.choices
+--     in
+--       { d | choices = newChoices }
 
-newMode : Mode -> Data -> Data
+newMode : Mode -> Model ->Model 
 newMode m d = 
   { d | mode = m}
 
-setMode : Mode -> Data -> Data 
-setMode  mode data = 
-  data 
-   |> (\d -> if mode == Checkbox then d else deselectAllButFirst mode d) 
-   |> newMode mode
+-- setMode : Mode -> Model -> Model 
+-- setMode  mode data = 
+--   data 
+--    |> (\d -> if mode == Checkbox then d else deselectAllButFirst mode d) 
+--    |> newMode mode
 
-setChoices : List Choice -> Data -> Data 
+setChoices : List Choice -> Model -> Model 
 setChoices c d = 
   {d | choices = c}
 
-toggleChoice : String -> Bool -> Mode -> Choice -> Choice
-toggleChoice v selected mode choice = 
-  if choice.value == v then 
-    { choice | selected = not choice.selected}
-  else
-    let 
-      selected = if mode == Radio then False else choice.selected 
-    in 
-      { choice | selected = selected } 
+-- toggleChoice : String -> Bool -> Mode -> Choice -> Choice
+-- toggleChoice v selected mode choice = 
+--   if choice.value == v then 
+--     { choice | selected = not choice.selected}
+--   else
+--     let 
+--       selected = if mode == Radio then False else choice.selected 
+--     in 
+--       { choice | selected = selected } 
 
 type alias MakeMsg msg = (String -> Bool -> msg)
 
-radio : String -> MakeMsg msg -> Bool -> Choice -> Html msg
-radio t makeMsg d choice =
-  div [] [
-    label []
-      [ input [ 
-        type_ t 
-      , disabled d 
-      , value choice.value
-      , onClick (makeMsg choice.value choice.selected) 
-      , checked choice.selected] []
-      , text choice.label
-      ]
-  ] 
+isSelected : String -> Session -> Bool 
+isSelected v session = member v session.value 
+
+radio : String -> MakeMsg msg -> Bool -> Session -> Choice -> Html msg
+radio t makeMsg d session choice=
+
+  let 
+    selected = isSelected choice.value session 
+  in 
+    div [] [
+      label []
+        [ input [ 
+          type_ t 
+        , disabled d 
+        , value choice.value
+        , onClick (makeMsg choice.value selected) 
+        , checked selected] []
+        , text choice.label
+        ]
+    ] 
 
 
-view : Data -> Bool -> (MakeMsg msg) -> Html msg 
+view : Model -> Bool -> (MakeMsg msg) -> Html msg 
 view data disabled makeMsg =
   let 
     choiceType = if data.mode == Radio then "radio" else "checkbox" 
@@ -97,4 +92,4 @@ view data disabled makeMsg =
     div [] 
         [ div [] [ text data.prompt ]
         , hr [] []
-        , div [] (map (radio choiceType makeMsg disabled) data.choices) ]
+        , div [] (map (radio choiceType makeMsg disabled data.session) data.choices) ]

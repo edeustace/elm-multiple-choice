3b39b841b8267fc576d19cc0dc84fae43e015faa
edeustace <ed.eustace@gmail.com>
2017-06-04 23:46:39 +0100

    tweaks

--- src/ToggleIcon.elm
+++ src/ToggleIcon.elm
@@ -10,7 +10,6 @@ import Process
 
 type Msg = 
   Toggle
-  | StateChange State
 
 type State = 
   Enter 
@@ -29,50 +28,49 @@ initialModel = {
   state = LeaveActive }
 
 update : Msg -> Model -> (Model, Cmd Msg) 
-update msg model = 
-  case (Debug.log ">>msg" msg) of 
-    Toggle ->
-      if model.state == Enter || model.state == Leave then 
-        -- do nothing
-        (model, Cmd.none)
-      else  
-        let
-          state = if model.toggled then Leave else Enter
-          endState = if model.toggled then LeaveActive else EnterActive 
-          t = Task.perform (\n -> (StateChange endState)) (Process.sleep <| 2000)  
-        in
-          ({model | state = state }, t )
-    StateChange s -> 
-      case s of 
-        Enter -> (model, Cmd.none)
-        Leave -> (model, Cmd.none)
-        EnterActive -> ({model | toggled = True, state = s}, Cmd.none)
-        LeaveActive -> ({model | toggled = False, state = s}, Cmd.none)
+update msg model =
+  let 
+    o = (Debug.log ">model" model)
+  in  
+    case (Debug.log ">>msg" msg) of 
+      Toggle ->
+        if model.state == Enter || model.state == Leave then 
+          -- do nothing
+          (model, Cmd.none)
+        else  
+          let
+            state = if model.toggled then LeaveActive else EnterActive 
+          in
+            ({model | toggled = not model.toggled, state = state }, Cmd.none )
 
 
 show model = 
   let 
     classes = case model.state of 
-      Leave -> ["enter"]
-      _ -> ["enter", "enter-active"]
+      Enter -> ["enter"]
+      EnterActive -> ["enter", "enter-active"]
+      Leave -> ["leave"]
+      LeaveActive -> ["leave", "leave-active"]
     expanded = (map (\c -> (c, True)) classes)
     clazz = classList expanded 
   in 
-    div [clazz, onClick Toggle] [text "show"]
+    div [clazz] [text "show"]
   
 hide model = 
   let 
     classes = case model.state of 
       Enter -> ["leave"]
-      _ -> ["leave", "leave-active"]
+      EnterActive -> ["leave", "leave-active"]
+      Leave -> ["enter"]
+      LeaveActive -> ["enter", "enter-active"]
     expanded = (map (\c -> (c, True)) classes)
     clazz = classList expanded 
   in 
-    div [clazz, onClick Toggle] [text "hide"]
+    div [clazz] [text "hide"]
 
 view : Model -> Html Msg
 view model =
-  div [onClick Toggle] [
+  div [class "toggle-icon-root", onClick Toggle] [
       show model, 
       hide model
     ]
